4ea93e4adefb63563099fe21a00c4fc86044dee3
a1115040996 <1115040996@qq.com>
2019-12-02 21:00:07 +0800

    React 初始框架搭建

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Module1 from './content/module1/module1';
+export default class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      user: 'zhangsan'
+    }
+    this.propConfig = {
+      name: 'props',
+      text: 'hello'
+    }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // 绑定 上下文对象
+    this.sendModule1Msg = this.sendModule1Msg.bind(this);
+  }
+
+  sendModule1Msg() {
+    console.log('ready to send!', this);
+  }
+
+  render(){
+      return (
+          <div className="App">
+              <header className="App-header">
+                  {this.state.user}
+              </header>
+
+            <div className="Module1Container">
+                <div
+                    className="send"
+                    onClick={this.sendModule1Msg}>
+                    <span>Click me to send Module1</span>
+                </div>
+
+                <Module1 data={this.propConfig}></Module1>
+            </div>
+          </div>
+      );
+  }
 }
 
-export default App;
